d4cdc3dfe51f52e7472da24a97ebb571eae1ff61
Gustavo Seabra <devguga87@gmail.com>
2025-07-31 22:28:11 -0300

    feat: cria metodos do controller

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,37 +1,49 @@
 package com.devsuperior.dsmeta.controllers;
 
+import com.devsuperior.dsmeta.dto.SalesRepportMinDTO;
+import com.devsuperior.dsmeta.dto.SummaryMinDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.services.SaleService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sales")
 public class SaleController {
 
-	@Autowired
-	private SaleService service;
-	
-	@GetMapping(value = "/{id}")
-	public ResponseEntity<SaleMinDTO> findById(@PathVariable Long id) {
-		SaleMinDTO dto = service.findById(id);
-		return ResponseEntity.ok(dto);
-	}
+    @Autowired
+    private SaleService service;
 
-	@GetMapping(value = "/report")
-	public ResponseEntity<?> getReport() {
-		// TODO
-		return null;
-	}
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<SaleMinDTO> findById(@PathVariable Long id) {
+        SaleMinDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
 
-	@GetMapping(value = "/summary")
-	public ResponseEntity<?> getSummary() {
-		// TODO
-		return null;
-	}
+    @GetMapping(value = "/report")
+    public ResponseEntity<Page<SalesRepportMinDTO>> getReport(
+            @RequestParam(name = "minDate", defaultValue = "") String minDate,
+            @RequestParam(name = "maxDate", defaultValue = "") String maxDate,
+            @RequestParam(name = "name", defaultValue = "") String name,
+            Pageable pageable
+    ) {
+        Page<SalesRepportMinDTO> dto = service.findRepport(minDate,maxDate,name,pageable);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(value = "/summary")
+    public ResponseEntity<List<SummaryMinDTO>> getSummary(
+            @RequestParam(name = "minDate", defaultValue = "") String minDate,
+            @RequestParam(name = "maxDate", defaultValue = "") String maxDate,
+            @RequestParam(name = "name", defaultValue = "") String name
+    ) {
+		List<SummaryMinDTO> dto = service.findSummary(minDate,maxDate,name);
+        return ResponseEntity.ok(dto);
+    }
 }
